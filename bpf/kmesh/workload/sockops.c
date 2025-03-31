d251a75232b5950e14ca0e784450e79185d48e5e
Yash Patel <yp969803@gmail.com>
2025-03-31 16:25:50 +0530

    some more logs

--- bpf/kmesh/workload/sockops.c
+++ bpf/kmesh/workload/sockops.c
@@ -167,7 +167,7 @@ int sockops_prog(struct bpf_sock_ops *skops)
     case BPF_SOCK_OPS_ACTIVE_ESTABLISHED_CB:
         if (!is_managed_by_kmesh(skops))
             break;
-        
+        bpf_printk("outbound connect established\n");
         observe_on_connect_established(skops->sk, sock_cookie, OUTBOUND);
         if (bpf_sock_ops_cb_flags_set(skops, BPF_SOCK_OPS_STATE_CB_FLAG) != 0
             || bpf_sock_ops_cb_flags_set(skops, BPF_SOCK_OPS_RETRANS_CB_FLAG) != 0
@@ -183,6 +183,7 @@ int sockops_prog(struct bpf_sock_ops *skops)
     case BPF_SOCK_OPS_PASSIVE_ESTABLISHED_CB:
         if (!is_managed_by_kmesh(skops) || skip_specific_probe(skops))
             break;
+        bpf_printk("inbound connect established\n");
         observe_on_connect_established(skops->sk, sock_cookie, INBOUND);
         if (bpf_sock_ops_cb_flags_set(skops, BPF_SOCK_OPS_STATE_CB_FLAG) != 0
             || bpf_sock_ops_cb_flags_set(skops, BPF_SOCK_OPS_RETRANS_CB_FLAG) != 0
@@ -193,13 +194,14 @@ int sockops_prog(struct bpf_sock_ops *skops)
         break;
 
     case BPF_SOCK_OPS_STATE_CB:
+        bpf_printk("sockops state change\n");
+        if (is_managed_by_kmesh(skops))
+            observe_on_status_change(skops->sk, skops->args[0]);
+        
         if (skops->args[1] == BPF_TCP_CLOSE) {
             clean_auth_map(skops);
             clean_dstinfo_map(skops);
         }
-        if (!is_managed_by_kmesh(skops))
-            break;
-        observe_on_status_change(skops->sk, skops->args[0]);
         break;
 
     case BPF_SOCK_OPS_RETRANS_CB:
