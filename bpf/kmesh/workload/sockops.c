94379d03770b0c17c887157cafdfab4f0cea796e
Yash Patel <yp969803@gmail.com>
2025-04-02 14:22:13 +0530

    some more changes

--- bpf/kmesh/workload/sockops.c
+++ bpf/kmesh/workload/sockops.c
@@ -162,20 +162,20 @@ int sockops_prog(struct bpf_sock_ops *skops)
     switch (skops->op) {
     case BPF_SOCK_OPS_TCP_CONNECT_CB:
         skops_handle_kmesh_managed_process(skops);
+        if (bpf_sock_ops_cb_flags_set(skops, BPF_SOCK_OPS_STATE_CB_FLAG) != 0) {
+            BPF_LOG(ERR, SOCKOPS, "set sockops cb failed!\n");
+        }
         break;
 
     case BPF_SOCK_OPS_ACTIVE_ESTABLISHED_CB:
         if (!is_managed_by_kmesh(skops))
             break;
         bpf_printk("outbound connect established\n");
-        if (bpf_sock_ops_cb_flags_set(skops, BPF_SOCK_OPS_STATE_CB_FLAG) != 0)
-             BPF_LOG(ERR, SOCKOPS, "set sockops cb failed!\n");
-        observe_on_connect_established(skops->sk, sock_cookie, OUTBOUND);
-        if (bpf_sock_ops_cb_flags_set(skops, BPF_SOCK_OPS_STATE_CB_FLAG) != 0
-            || bpf_sock_ops_cb_flags_set(skops, BPF_SOCK_OPS_RETRANS_CB_FLAG) != 0
-            || bpf_sock_ops_cb_flags_set(skops, BPF_SOCK_OPS_RTT_CB_FLAG) != 0) {
+        if (bpf_sock_ops_cb_flags_set(skops, BPF_SOCK_OPS_STATE_CB_FLAG) != 0) {
             BPF_LOG(ERR, SOCKOPS, "set sockops cb failed!\n");
         }
+        observe_on_connect_established(skops->sk, sock_cookie, OUTBOUND);
+        
         __u64 *current_sk = (__u64 *)skops->sk;
         struct bpf_sock_tuple *dst = bpf_map_lookup_elem(&map_of_orig_dst, &current_sk);
         if (dst != NULL)
@@ -186,14 +186,11 @@ int sockops_prog(struct bpf_sock_ops *skops)
         if (!is_managed_by_kmesh(skops) || skip_specific_probe(skops))
             break;
         bpf_printk("inbound connect established\n");
-        if (bpf_sock_ops_cb_flags_set(skops, BPF_SOCK_OPS_STATE_CB_FLAG) != 0)
-            BPF_LOG(ERR, SOCKOPS, "set sockops cb failed!\n");
-        observe_on_connect_established(skops->sk, sock_cookie, INBOUND);
-        if (bpf_sock_ops_cb_flags_set(skops, BPF_SOCK_OPS_STATE_CB_FLAG) != 0
-            || bpf_sock_ops_cb_flags_set(skops, BPF_SOCK_OPS_RETRANS_CB_FLAG) != 0
-            || bpf_sock_ops_cb_flags_set(skops, BPF_SOCK_OPS_RTT_CB_FLAG) != 0) {
+        if (bpf_sock_ops_cb_flags_set(skops, BPF_SOCK_OPS_STATE_CB_FLAG) != 0) {
             BPF_LOG(ERR, SOCKOPS, "set sockops cb failed!\n");
         }
+        observe_on_connect_established(skops->sk, sock_cookie, INBOUND);
+        
         auth_ip_tuple(skops);
         break;
 
--- pkg/controller/telemetry/metric.go
+++ pkg/controller/telemetry/metric.go
@@ -378,10 +378,10 @@ func (m *MetricController) Run(ctx context.Context, mapOfTcpInfo *ebpf.Map) {
 				log.Errorf("ringbuf reader FAILED to read, err: %v", err)
 				continue
 			}
-			// if len(rec.RawSample) != int(unsafe.Sizeof(connectionDataV4{})) {
-			// 	log.Errorf("wrong length %v of a msg, should be %v", len(rec.RawSample), int(unsafe.Sizeof(connectionDataV4{})))
-			// 	continue
-			// }
+			if len(rec.RawSample) != int(unsafe.Sizeof(connectionDataV4{})) {
+				log.Errorf("wrong length %v of a msg, should be %v", len(rec.RawSample), int(unsafe.Sizeof(connectionDataV4{})))
+				continue
+			}
 			connectType := binary.LittleEndian.Uint32(rec.RawSample)
 			originInfo := rec.RawSample[unsafe.Sizeof(connectType):]
 			buf := bytes.NewBuffer(originInfo)
@@ -467,6 +467,7 @@ func buildV4Metric(buf *bytes.Buffer, tcp_conns map[uint64]connMetric) (requestM
 		totalRetrans:  connectData.statistics.Retransmits,
 		packetLost:    connectData.statistics.LostPackets,
 	}
+	
 
 	if data.state == TCP_CLOSTED {
 		delete(tcp_conns, connectData.ConnId)
