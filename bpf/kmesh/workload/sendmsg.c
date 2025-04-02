3fa2ba91f4f144ecb8005b505561e563828ebd14
Yash Patel <yp969803@gmail.com>
2025-04-02 15:42:18 +0530

    some more logs

--- bpf/kmesh/workload/sendmsg.c
+++ bpf/kmesh/workload/sendmsg.c
@@ -202,16 +202,16 @@ int sendmsg_prog(struct sk_msg_md *msg)
     // encode org dst addr
     encode_metadata_org_dst_addr(msg, &off, (msg->family == AF_INET));
 
-    struct bpf_sock *sk = msg->sk;
-    bpf_printk("send msg triggered \n");
-    if (sk) {
-        if (is_managed_by_kmesh_skmsg(msg)) {
-            observe_on_send(sk, msg->size);
-            report_after_threshold_tm(sk);
-        }
-    } else {
-        BPF_LOG(ERR, KMESH, "sk_lookup success\n");
-    }
+    // struct bpf_sock *sk = msg->sk;
+
+    // if (sk) {
+    //     if (is_managed_by_kmesh_skmsg(msg)) {
+    //         observe_on_send(sk, msg->size);
+    //         report_after_threshold_tm(sk);
+    //     }
+    // } else {
+    //     BPF_LOG(ERR, KMESH, "sk_lookup success\n");
+    // }
 
     return SK_PASS;
 }
--- bpf/kmesh/workload/sockops.c
+++ bpf/kmesh/workload/sockops.c
@@ -195,24 +195,24 @@ int sockops_prog(struct bpf_sock_ops *skops)
         auth_ip_tuple(skops);
         break;
 
-    case BPF_SOCK_OPS_STATE_CB:
+    // case BPF_SOCK_OPS_STATE_CB:
         
         
-        observe_on_status_change(skops->sk, skops->args[0]);
+    //     observe_on_status_change(skops->sk, skops->args[0]);
         
-        if (skops->args[1] == BPF_TCP_CLOSE) {
-            clean_auth_map(skops);
-            clean_dstinfo_map(skops);
-        }
-        break;
+    //     if (skops->args[1] == BPF_TCP_CLOSE) {
+    //         clean_auth_map(skops);
+    //         clean_dstinfo_map(skops);
+    //     }
+    //     break;
 
-    case BPF_SOCK_OPS_RETRANS_CB:
-        observe_on_retransmit(skops->sk);
-        break;
+    // case BPF_SOCK_OPS_RETRANS_CB:
+    //     observe_on_retransmit(skops->sk);
+    //     break;
 
-    case BPF_SOCK_OPS_RTT_CB:
-        observe_on_rtt(skops->sk);
-        break;
+    // case BPF_SOCK_OPS_RTT_CB:
+    //     observe_on_rtt(skops->sk);
+    //     break;
 
     default:
         break;
