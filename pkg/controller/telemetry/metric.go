b6ab28c16cc313e15081248c88f4068a0edb31e1
Yash Patel <yp969803@gmail.com>
2025-04-02 17:24:51 +0530

    some more logs
    Signed-off-by: Yash Patel <yp969803@gmail.com>

--- pkg/controller/telemetry/metric.go
+++ pkg/controller/telemetry/metric.go
@@ -434,7 +434,7 @@ func buildV4Metric(buf *bytes.Buffer, tcp_conns map[uint64]connMetric) (requestM
 	data.src[0] = connectData.SrcAddr
 	data.dst[0] = connectData.DstAddr
 	fmt.Print("directopn: ", connectData.Direction)
-	data.direction = connectData.Direction
+	data.direction = uint32(2)
 	data.dstPort = connectData.DstPort
 	data.srcPort = connectData.SrcPort
 
@@ -485,7 +485,7 @@ func buildV6Metric(buf *bytes.Buffer, tcp_conns map[uint64]connMetric) (requestM
 	}
 	data.src = connectData.SrcAddr
 	data.dst = connectData.DstAddr
-	data.direction = connectData.Direction
+	data.direction = uint32(2)
 	data.dstPort = connectData.DstPort
 	data.srcPort = connectData.SrcPort
 
