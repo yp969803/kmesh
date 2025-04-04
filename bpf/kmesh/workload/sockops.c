ecebedce87b3aa56085296dd1cde3df5813409d2
Yash Patel <yp969803@gmail.com>
2025-04-05 01:40:04 +0530

    some more logs

--- bpf/kmesh/workload/sockops.c
+++ bpf/kmesh/workload/sockops.c
@@ -203,8 +203,10 @@ int sockops_prog(struct bpf_sock_ops *skops)
             break;
         }
         
+        observe_on_status_change(skops->sk, skops->args[1]);
+        
         if (skops->args[1] == BPF_TCP_CLOSE) {
-            observe_on_status_change(skops->sk, skops->args[1]);
+            
             clean_auth_map(skops);
             clean_dstinfo_map(skops);
         }
