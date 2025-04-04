89eb36e77015ee42b0630a2dc73be80c2b8fb6a8
Yash Patel <yp969803@gmail.com>
2025-04-04 17:02:48 +0530

    some more logs

--- bpf/kmesh/workload/sockops.c
+++ bpf/kmesh/workload/sockops.c
@@ -161,9 +161,6 @@ int sockops_prog(struct bpf_sock_ops *skops)
 
     switch (skops->op) {
     case BPF_SOCK_OPS_TCP_CONNECT_CB:
-        if (bpf_sock_ops_cb_flags_set(skops, BPF_SOCK_OPS_STATE_CB_FLAG) != 0) {
-            bpf_printk("set sockops cb failed!\n");
-        }
         skops_handle_kmesh_managed_process(skops);
         break;
 
@@ -171,15 +168,12 @@ int sockops_prog(struct bpf_sock_ops *skops)
         if (!is_managed_by_kmesh(skops))
             break;
         bpf_printk("outbound connect established outbound %llu \n", sock_cookie);
-        if (bpf_sock_ops_cb_flags_set(skops, BPF_SOCK_OPS_STATE_CB_FLAG) != 0) {
+        if (bpf_sock_ops_cb_flags_set(skops, 
+            BPF_SOCK_OPS_STATE_CB_FLAG | 
+            BPF_SOCK_OPS_RETRANS_CB_FLAG | 
+            BPF_SOCK_OPS_RTT_CB_FLAG) != 0) {
             BPF_LOG(ERR, SOCKOPS, "set sockops cb failed!\n");
         }
-        // if (bpf_sock_ops_cb_flags_set(skops, BPF_SOCK_OPS_RETRANS_CB_FLAG) != 0) {
-        //     BPF_LOG(ERR, SOCKOPS, "set sockops cb failed!\n");
-        // }
-        // if (bpf_sock_ops_cb_flags_set(skops, BPF_SOCK_OPS_RTT_CB_FLAG) != 0) {
-        //     BPF_LOG(ERR, SOCKOPS, "set sockops cb failed!\n");
-        // }
         observe_on_connect_established(skops->sk, sock_cookie, OUTBOUND);
         
         __u64 *current_sk = (__u64 *)skops->sk;
@@ -191,14 +185,11 @@ int sockops_prog(struct bpf_sock_ops *skops)
     case BPF_SOCK_OPS_PASSIVE_ESTABLISHED_CB:
         if (!is_managed_by_kmesh(skops) || skip_specific_probe(skops))
             break;
-        if (bpf_sock_ops_cb_flags_set(skops, BPF_SOCK_OPS_STATE_CB_FLAG) != 0) {
-            BPF_LOG(ERR, SOCKOPS, "set sockops cb failed!\n");
-        }
-        if (bpf_sock_ops_cb_flags_set(skops, BPF_SOCK_OPS_RETRANS_CB_FLAG) != 0) {
-            BPF_LOG(ERR, SOCKOPS, "set sockops cb failed!\n");
-        }
-        if (bpf_sock_ops_cb_flags_set(skops, BPF_SOCK_OPS_RTT_CB_FLAG) != 0) {
-            BPF_LOG(ERR, SOCKOPS, "set sockops cb failed!\n");
+        if (bpf_sock_ops_cb_flags_set(skops, 
+                BPF_SOCK_OPS_STATE_CB_FLAG | 
+                BPF_SOCK_OPS_RETRANS_CB_FLAG | 
+                BPF_SOCK_OPS_RTT_CB_FLAG) != 0) {
+                BPF_LOG(ERR, SOCKOPS, "set sockops cb failed!\n");
         }
         bpf_printk(" connect established inbound %llu ", sock_cookie);
         observe_on_connect_established(skops->sk, sock_cookie, INBOUND);
@@ -211,27 +202,27 @@ int sockops_prog(struct bpf_sock_ops *skops)
         if(!is_managed_by_kmesh(skops)) {
             break;
         }
-        
+        observe_on_status_change(skops->sk, skops->args[1]);
         if (skops->args[1] == BPF_TCP_CLOSE) {
-            observe_on_status_change(skops->sk, skops->args[1]);
+            
             clean_auth_map(skops);
             clean_dstinfo_map(skops);
         }
         break;
 
-    // case BPF_SOCK_OPS_RETRANS_CB:
-    //     if(!is_managed_by_kmesh(skops)) {
-    //        break;
-    //     }
-    //     observe_on_retransmit(skops->sk);
-    //     break;
+    case BPF_SOCK_OPS_RETRANS_CB:
+        if(!is_managed_by_kmesh(skops)) {
+           break;
+        }
+        observe_on_retransmit(skops->sk);
+        break;
 
-    // case BPF_SOCK_OPS_RTT_CB:
-    //     if(!is_managed_by_kmesh(skops)) {
-    //       break;
-    //     }
-    //     observe_on_rtt(skops->sk);
-    //     break;
+    case BPF_SOCK_OPS_RTT_CB:
+        if(!is_managed_by_kmesh(skops)) {
+          break;
+        }
+        observe_on_rtt(skops->sk);
+        break;
 
     default:
         break;
